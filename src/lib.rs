bf55ec89de6fa82261c2a2d4104631aece1707fe
evopen <520dhh@gmail.com>
2020-12-29 14:18:44 +0800

    support deriving json schema

--- src/lib.rs
+++ src/lib.rs
@@ -106,6 +106,7 @@ pub fn pib<V: Into<u64>>(size: V) -> u64 {
 /// Byte size representation
 #[derive(Copy, Clone, PartialEq, PartialOrd, Eq, Ord, Default)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "schemars", derive(JsonSchema))]
 pub struct ByteSize(pub u64);
 
 impl ByteSize {
